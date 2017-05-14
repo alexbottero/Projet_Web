4ff3d1fb823139f3decf13d2f8687e39d7c3511a
Alexandre.BOTTERO <alex.bottero@hotmail.fr>
2017-05-14 23:50:38 +0200

    index

---
+++ Configs/Config_css.php
@@ -0,0 +1,13 @@
+
+/**
+ * Created by PhpStorm.
+ * User: alexandre
+ * Date: 14/05/2017
+ * Time: 22:30
+ */
+<!--Import Google Icon Font-->
+<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!--Import materialize.css-->
+<link type="text/css" rel="stylesheet" href="../css/materialize.css"  media="screen,projection"/>
+<!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
--- Vues/Vue_Accueil.php
+++ Vues/Vue_Accueil.php
@@ -1,7 +1,32 @@
-<?php
+
 /**
  * Created by PhpStorm.
  * User: alexandre
  * Date: 14/05/2017
  * Time: 16:30
  */
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projet Web</title>
+    <?php include ('../Configs/Config_css.php'); ?>
+</head>
+<body>
+<div class="fixed-action-btn horizontal">
+    <a class="btn-floating btn-large red">
+        <i class="large material-icons">mode_edit</i>
+    </a>
+    <ul>
+        <li><a class="btn-floating red"><i class="material-icons">Covoiturages</i></a></li>
+        <li><a class="btn-floating yellow darken-1"><i class="material-icons">Appartements</i></a></li>
+        <li><a class="btn-floating green"><i class="material-icons">Trocs</i></a></li>
+    </ul>
+</div>
+
+<?php include ('../Configs/Config_js.php'); ?>
+</body>
+</html>
